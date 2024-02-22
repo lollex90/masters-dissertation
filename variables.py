0c10527db452e5b2d5b58b12ee976c41bd61d3fc
lollex90 <j.m.terlikowski@lse.ac.uk>
2024-02-22 17:09:29 +0000

    clean gdp data

--- variables.py
+++ variables.py
@@ -26,6 +26,67 @@ region_names = ['Kherson Oblast',
  'Kyiv',
  'Sevastopol']
 
+region_names_gdp = ['Autonomous Republic of Crimea',
+ 'Vinnytsya',
+ 'Volyn',
+ 'Dnipropetrovsk',
+ 'Donetsk',
+ 'Zhytomyr',
+ 'Zakarpattya',
+ 'Zaporizhzhya',
+ 'Ivano-Frankivsk',
+ 'Kyiv',
+ 'Kirovohrad',
+ 'Luhansk',
+ 'Lviv',
+ 'Mykolayiv',
+ 'Odesa',
+ 'Poltava',
+ 'Rivne',
+ 'Sumy',
+ 'Ternopyl',
+ 'Kharkiv',
+ 'Kherson',
+ 'Khmelnytskiy',
+ 'Cherkasy',
+ 'Chernivtsi',
+ 'Chernihiv',
+ 'Kyiv city',
+ 'Sevastopol city']
+
+# create a dictionary to map the region names
+region_map = {
+    'Kherson': 'Kherson Oblast',
+    'Volyn': 'Volyn Oblast',
+    'Rivne': 'Rivne Oblast',
+    'Zhytomyr': 'Zhytomyr Oblast',
+    'Kyiv': 'Kyiv Oblast',
+    'Chernihiv': 'Chernihiv Oblast',
+    'Sumy': 'Sumy Oblast',
+    'Kharkiv': 'Kharkiv Oblast',
+    'Luhansk': 'Luhansk Oblast',
+    'Donetsk': 'Donetsk Oblast',
+    'Zaporizhzhya': 'Zaporizhia Oblast',
+    'Lviv': 'Lviv Oblast',
+    'Ivano-Frankivsk': 'Ivano-Frankivsk Oblast',
+    'Zakarpattya': 'Zakarpattia Oblast',
+    'Ternopyl': 'Ternopil Oblast',
+    'Chernivtsi': 'Chernivtsi Oblast',
+    'Odesa': 'Odessa Oblast',
+    'Mykolayiv': 'Mykolaiv Oblast',
+    'Autonomous Republic of Crimea': 'Autonomous Republic of Crimea',
+    'Vinnytsya': 'Vinnytsia Oblast',
+    'Khmelnytskiy': 'Khmelnytskyi Oblast',
+    'Cherkasy': 'Cherkasy Oblast',
+    'Poltava': 'Poltava Oblast',
+    'Dnipropetrovsk': 'Dnipropetrovsk Oblast',
+    'Kirovohrad': 'Kirovohrad Oblast',
+    'Kyiv city': 'Kyiv',
+    'Sevastopol city': 'Sevastopol'
+}
+
 years = ["2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023"]
 
-composites = ["AllAngle_Composite_Snow_Covered", "AllAngle_Composite_Snow_Free"]
+composites = ["AllAngle_Composite_Snow_Covered", "AllAngle_Composite_Snow_Free", 
+              "NearNadir_Composite_Snow_Covered", "NearNadir_Composite_Snow_Free", 
+              "OffNadir_Composite_Snow_Covered", "OffNadir_Composite_Snow_Free"]
